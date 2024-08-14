741b2fe4ff82905b8b198f7d13448760de43a2cf
Elizabeth Kenneally <elizabeth.kenneally@childmind.org>
2024-08-14 09:58:06 -0400

    bugfix workflow

--- run_longitudinal.py
+++ run_longitudinal.py
@@ -2,19 +2,19 @@
 # Separated and de-nipype'd C-PAC longitudinal workflow
 # Anatomical preproc runs before this in C-PAC
 
-import subprocess
 import os
 
 from longitudinal_reg.longitudinal_utils import (
     subject_specific_template,
     warp_longitudinal_T1w_to_template,
-    warp_longitudinal_seg_to_T1w
+    warp_longitudinal_seg_to_T1w,
+    fs_generate_template
 )
 
 from longitudinal_reg.register import (
     mask_longitudinal_T1w_brain,
-    register_ANTS_anat_to_template,
-    register_fsl_anat_to_template,
+    register_ANTs_anat_to_template,
+    register_FSL_anat_to_template,
     overwrite_transform_anat_to_template,
     register_symmetric_ANTs_anat_to_template,
     register_symmetric_FSL_anat_to_template
@@ -25,55 +25,67 @@ from longitudinal_reg.segment import (
 )
 
 
-def run_workflow(config, out_dir):
+def run_workflow(config, cpac_dir, out_dir, subject_id):
 
-    # cd into output dir
-    # maybe cd into subdir for each session???
 
     cpac_dirs = []
-    sub_list = []
-    cpac_dir = out_dir
-    
-    cpac_dirs.append(os.path.join(cpac_dir, "anat"))
     sessions = []
+    cpac_dirs.append(os.path.join(cpac_dir, "anat"))
+
+    for root, dirs, files in os.walk(cpac_dir):
+        # The root is the current folder, dirs are the subfolders in the current folder
+        for dir_name in dirs:
+            sessions.append(dir_name)
+        break
+    print(sessions)
+    # To change template generation algorithm
+    use_fs = False
 
     # loop over the different anat preproc strategies
     strats_brain_dct = {}
     strats_head_dct = {}
-    for cpac_dir in cpac_dirs:
-        if os.path.isdir(cpac_dir):
-            for filename in os.listdir(cpac_dir):
-                if "T1w.nii" in filename:
-                    for tag in filename.split("_"):
-                        if "desc-" in tag and "preproc" in tag:
-                            if tag not in strats_brain_dct:
-                                strats_brain_dct[tag] = []
-                            strats_brain_dct[tag].append(
-                                os.path.join(cpac_dir, filename)
-                            )
-                            if tag not in strats_head_dct:
-                                strats_head_dct[tag] = []
-                            head_file = filename.replace(tag, "desc-head")
-                            strats_head_dct[tag].append(
-                                os.path.join(cpac_dir, head_file)
-                            )
-
+    for dir in os.listdir(cpac_dir):
+        if os.path.isdir(os.path.join(cpac_dir, dir)):
+            anat_dir = os.path.join(cpac_dir, dir, "anat")
+            if os.path.isdir(anat_dir):
+                for filename in os.listdir(anat_dir):
+                    if "T1w.nii" in filename:
+                        for tag in filename.split("_"):
+                            if "desc-" in tag and "preproc" in tag:
+                                if tag not in strats_brain_dct:
+                                    strats_brain_dct[tag] = []
+                                strats_brain_dct[tag].append(
+                                    os.path.join(dir, filename)
+                                )
+                                if tag not in strats_head_dct:
+                                    strats_head_dct[tag] = []
+                                head_file = filename.replace(tag, "desc-head")
+                                strats_head_dct[tag].append(
+                                    os.path.join(dir, head_file)
+                                )
+    print(strats_head_dct)
+    print(strats_brain_dct)
     for strat in strats_brain_dct.keys():
 
         # This will generate the longitudinal template 
-        brain_template, skull_template, output_brain_list, \
-            output_skull_list, warp_list = subject_specific_template( 
-                input_brain_list=strats_brain_dct[strat], 
-                input_skull_list=strats_head_dct[strat],
-                avg_method=config.longitudinal_template_generation["average_method"],
-                dof=config.longitudinal_template_generation["dof"],
-                interp=config.longitudinal_template_generation["interp"],
-                cost=config.longitudinal_template_generation["cost"],
-                convergence_threshold=config.longitudinal_template_generation[
-                    "convergence_threshold"],
-                thread_pool=config.longitudinal_template_generation["thread_pool"],
-                unique_id_list=list(sessions))
 
+        if use_fs:
+            warp_list, brain_template = fs_generate_template()
+        else:
+            brain_template, skull_template, output_brain_list, \
+                output_skull_list, warp_list = subject_specific_template( 
+                    input_brain_list=strats_brain_dct[strat], 
+                    input_skull_list=strats_head_dct[strat],
+                    avg_method=config.longitudinal_template_generation["average_method"],
+                    dof=config.longitudinal_template_generation["dof"],
+                    interp=config.longitudinal_template_generation["interp"],
+                    cost=config.longitudinal_template_generation["cost"],
+                    convergence_threshold=config.longitudinal_template_generation[
+                        "convergence_threshold"],
+                    thread_pool=config.longitudinal_template_generation["thread_pool"],
+                    unique_id_list=list(sessions))
+
+        # TODO: does this happen in fs command? I don't think so
         longitudinal_brain_mask = mask_longitudinal_T1w_brain(brain_template)
         
         #TODO - how to deal w templates?? just paths for now
@@ -89,18 +101,18 @@ def run_workflow(config, out_dir):
 
 
         #rpool.get_data("T1w-brain-template-mask") if strat_pool.check_rpool("T1w-brain-template-mask") else None
-        # lesion_mask = 
-        #strat_pool.get_data("label-lesion_mask") if strat_pool.check_rpool("label-lesion_mask") else None
+        # lesion_mask = strat_pool.get_data("label-lesion_mask") if strat_pool.check_rpool("label-lesion_mask") else None
 
         # registration - once for each session
-        for session in sub_list:
+        for session in sessions:
             # cd into session folder
+            # pipe num
             if config.registration_workflows.anatomical_registration["run"] and \
                 config.registration_workflows.anatomical_registration["using"] == "ANTS":
                 if config.voxel_mirrored_homotopic_connectivity["run"]:
                     reg_outputs = register_symmetric_ANTs_anat_to_template()
                 else:
-                    reg_outputs = register_ANTS_anat_to_template(config, input_brain=brain_template, input_head=skull_template,
+                    reg_outputs = register_ANTs_anat_to_template(config, input_brain=brain_template, input_head=skull_template,
                             input_mask=longitudinal_brain_mask, reference_brain=reference_brain, 
                             reference_head=reference_head, reference_mask=reference_mask, 
                             lesion_mask=None)
@@ -111,27 +123,44 @@ def run_workflow(config, out_dir):
                 if config.voxel_mirrored_homotopic_connectivity["run"]:
                     reg_outputs = register_symmetric_FSL_anat_to_template()
                 else:
-                    reg_outputs = register_fsl_anat_to_template(config, skull_template, reference_head, reference_brain, template_ref_mask)
+                    reg_outputs = register_FSL_anat_to_template(config, skull_template, reference_head, reference_brain, template_ref_mask)
             if config.registration_workflows.anatomical_registration.overwrite_transform["run"]:
                 overwrite_transform_anat_to_template() 
             
             # tissue segmentation
             if config.segmentation["run"] and config.segmentation.tissue_segmentation["using"] == "FSL-FAST":
-                tissue_seg_fsl_fast()
+                labels = tissue_seg_fsl_fast(config, pipe_num=session)
 
-
+        transform = reg_outputs["from-longitudinal_to-template_mode-image_xfm"]
+    
+        seg_transform = reg_outputs["from-longitudinal_to-T1w_mode-image_desc-linear_xfm"]
         # begin single-session stuff again
-        for session in sub_list:
-            warp_longitudinal_T1w_to_template()
-            warp_longitudinal_seg_to_T1w()
+        for session in sessions:
+            brain_template = warp_longitudinal_T1w_to_template(config, pipe_num=session, input_image=brain_template, 
+                                                                                reference=reference_brain, transform=transform)
+            label_outputs = warp_longitudinal_seg_to_T1w(config, pipe_num=session, images=labels, reference=reference_brain, transform=seg_transform)
 
 
 def main():
-    config = "/path/to/config"
-    out_dir = "/cpac/preproc/outputs"
+    config = "/home/c-pac_user/CMI/longitudinal/config.yml"
+    cpac_dir = "/home/c-pac_user/CMI/data/pipeline_cpac-default-pipeline"
+    out_dir = "/home/c-pac_user/CMI/longitudinal"
     
-    # for subject in out_dir:
-    run_workflow(config, out_dir)
+    print(out_dir)
+    print(os.path.isdir(cpac_dir))
+    for root, dirs, files in os.walk(cpac_dir):
+        for dir_name in dirs:
+            print('dir name', dir_name)
+            full_path = os.path.join(root, dir_name)
+            output_subfolder = os.path.join(out_dir, dir_name)
+            os.makedirs(output_subfolder, exist_ok=True)
+            
+            # Change the working directory to the output folder
+            # THis is necessary for the os.cwd calls in cpac functions
+            os.chdir(output_subfolder)
+            run_workflow(config, full_path, output_subfolder, dir_name)
+            os.chdir(out_dir)
+        break
 
 if __name__ == "__main__":
     main()
